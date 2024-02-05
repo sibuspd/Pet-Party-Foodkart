1c82ab34aef0b242f97f728d61227c49a6ec8f73
sibuspd <sibuspd@gmail.com>
2024-02-05 22:58:01 +0530

    Online Status Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useStatus from "../utils/useStatus";
 const Header = () => {
     const [btnNameReact,setbtnNameReact] = useState("Login");
+    const onlineStatus = useStatus(); 
     return (
         <div className="Header">
             <div className="Logo-container">
@@ -10,6 +12,7 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
+                    <li>{onlineStatus?"🟢" : "🔴"}</li>
                     <li><Link to="/">Home</Link></li>
                     <li>Cuisines</li>   
                     <li><Link to="/about">About Us</Link></li>
