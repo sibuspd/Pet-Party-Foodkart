1b7aa29535c25fc49387cbb38df83379d1ca59c3
sibuspd <sibuspd@gmail.com>
2024-02-07 23:31:10 +0530

    CSS Extended

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
     return (
         <div className="flex justify-between shadow-lg mt-1 mb-3 px-5 bg-slate-200 
         sm:bg-pink-400 lg:bg-green-300">
-            <div className="Logo-container w-15 mt-2">
-                <img className="w-20 rounded-full border border-solid border-l-amber-950" src={LOGO_URL}/>
+            <div className="Logo-container w-15 mt-2 hover:bg-green-600 rounded-lg"><Link to="/">
+                <img className="w-20 rounded-full border border-solid border-l-amber-950" src={LOGO_URL}/></Link>
             </div>
             <div className="flex items-center">
                 <ul className="flex p-8 m-1">
@@ -19,7 +19,7 @@ const Header = () => {
                     <li className="px-4"><Link to="/about">About Us</Link></li>
                     <li className="px-4">Cart</li>
                     <li className="px-4"><Link to="/contact">Contact</Link></li>
-                    <li className="px-4"><button className="login" onClick={()=>{btnNameReact==="Login"?
+                    <li className=" hover:bg-white px-4 pb-1 bg-sky-100 rounded-3xl shadow-orange-800 font-semibold"><button className="login" onClick={()=>{btnNameReact==="Login"?
                         setbtnNameReact("Logout"):setbtnNameReact("Login");}}>{btnNameReact}</button></li>   
                 </ul>
             </div>
