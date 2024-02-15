5a7a6812dbb3d7903669575090377e1d82d00682
sibuspd <sibuspd@gmail.com>
2024-02-15 16:02:44 +0530

    Using Context to Make Global Changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {lazy, Suspense} from "react";
+import React, {lazy, Suspense, useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -7,15 +7,27 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+import UserContext from "./utils/UserContext";
 
 const Cuisines = lazy(()=> import("./components/Cuisines"));
 
 const AppLayout = () => {
+  const [userName, setUserName] = useState();
+  
+  useEffect(()=>{
+    const data = {
+      name: "Sabyasachi Sahani",
+    };
+    setUserName(data.name);
+  },[]);
+
   return (
-    <div className="app">
-      <Header />
-      <Outlet />
-    </div>
+    <UserContext.Provider value={{loggedInUser: userName, setUserName}}>
+      <div className="app">
+        <Header />
+        <Outlet />  
+      </div>
+    </UserContext.Provider>
   );
 };
 
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,10 @@
 import RestaurantCard, { withOpenLabel } from "./RestaurantCard";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Shimmer from "./Shimmer";
 import { API_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import useStatus from "../utils/useStatus";
+import UserContext from "../utils/UserContext";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants]= useState([]); // The second parameter updates the first variable when it is called elsewhere. At this point, 'listOfRestaurant' is blank i.e., a blank array
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
@@ -11,8 +12,6 @@ const Body = () => {
   const [searchText,setSearchText] = useState("");
 
   const RestaurantCardOpen = withOpenLabel(RestaurantCard); 
-
-  console.log(listOfRestaurants);
   
   useEffect(()=>{fetchData(); const timer = setInterval(()=>{console.log("Rendering")},1000);
                   return(()=> {clearInterval(timer); console.log("useEffect withdrawn")});},[]); 
@@ -22,7 +21,7 @@ const Body = () => {
     const data = await  fetch(API_URL); 
     // awaits further execution till the API fetches data from cross-origin source.
     const json = await data.json();
-    console.log(json); // Just to record what is being fetched from remote server in JSON format
+    // console.log(json); // Just to record what is being fetched from remote server in JSON format
     
     const restaurants = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || 
     json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
@@ -36,6 +35,8 @@ const Body = () => {
 
   const onlineStatus = useStatus();
 
+  const { loggedInUser,setUserName } = useContext(UserContext);
+
   if(onlineStatus===false) return <h1>You are offline. Please check your internet connection</h1>
   else return(listOfRestaurants.length===0)? // When the array is empty (Conditional Rendering) 
   <Shimmer/>: // '?:' combination is ternary operator
@@ -45,14 +46,21 @@ const Body = () => {
               <div className="search m-1 p-1 inline-flex">
                 <input type="text" placeholder="Search here" className="border border-solid border-blue-950" value={searchText}
                 onChange={(e)=> {setSearchText(e.target.value);}}/>
-                </div>
+              </div>
               
               <button className="searchBtn px-5 mx-3 bg-green-300 rounded-xl" onClick={()=>{
                 const filtered_Restaurant = listOfRestaurants.filter((res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase()));
                 setFilteredRestaurant(filtered_Restaurant);}}>Search</button>
 
-              <button className="filter-btn px-5 mx-4 my-2 bg-blue-200 rounded-xl" onClick={()=>{const filteredlist = listOfRestaurants.filter((res)=>res.info.avgRating>4.3);
-              setListOfRestaurants(filteredlist);}}> Top Rated </button>
+              <button className="filter-btn px-5 mx-4 my-2 bg-blue-200 rounded-xl" onClick={()=>{const filteredlist = listOfRestaurants.filter((res)=>res?.info.avgRating>4.3);
+              setFilteredRestaurant(filteredlist);}}> Top Rated </button>
+
+              <label className="bg-slate-300 font-semibold rounded-md p-1 ml-96" 
+              >Customer's Name:</label>
+              <input className="border border-black mx-4" 
+              value={loggedInUser}
+              onChange={(e)=>setUserName(e.target.value)}></input>
+              
             </div>
 
             <div className="ml-10 mt-2 shadow-md res-container flex flex-wrap">
