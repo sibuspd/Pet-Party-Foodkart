c8ccd80603051e885502f768167e69bc88e1fc74
sibuspd <sibuspd@gmail.com>
2024-02-18 18:39:58 +0530

    Cart Features Added

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,8 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice"
 
-const { configureStore } = require("@reduxjs/toolkit");
-
 const appStore = configureStore({
     reducer: {
         cart: cartReducer,
