44d3c03d2c17c93318142dd1c75521e9fb8951da
sibuspd <sibuspd@gmail.com>
2024-03-18 20:22:40 +0530

    Removing local configs - Babel, Jest, Parcel

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,13 @@
 const Contact = () => {
   return (
     <div>
-      <h1> Contact Us </h1>
-      <h2>8234008357</h2>
-      <p>sibuspd@gmail.com</p>
+      <h1 className="font-bold text-3xl p-4 m-2">Contact Us </h1>
+      <form>
+        <input className="border border-black p-2 m-2" type="text" placeholder="Full Name"></input>
+        <input className="border border-black p-2 m-2" type="text" placeholder="Message"></input>
+        <button className="border border-black p-2 m-2 bg-slate-300 rounded-sm">Submit</button>
+
+      </form>
     </div>
   );
 };
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react"
+import Contact from "../Contact";
+
+test("Loading of Contact component",()=>{
+    render(<Contact/>);
+})
